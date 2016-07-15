bfcf9e28a4bc6fcbe522192936236b1a87042855
TaylanT <tokan@ift.uni-hannover>
2016-07-15 09:35:04 +0200

    sync new

--- gesamtdatenbank3.py
+++ gesamtdatenbank3.py
@@ -8,12 +8,45 @@ import numpy as np
 import pandas as pd
 from influxdb import DataFrameClient
 # from influxdb.exceptions import InfluxDBClientError
+from dirsync import sync
+sync('/home/ttaylan/Studenten/HT-Waermepumpe/Messdaten',
+    '/home/ttaylan/Dokumente/Datenbank/NeueDaten/', 'sync')
+
+
+def abfrage_datenbank(filename, messung, tag):
+    """Dynamische Query fuer abrage ob eintrag in Datenbank existiert."""
+    return"select COP from %s where Filename=\'%s\'" % (messung, tag)
+
+def find_datumzeit(filename,status):
+    """Zustand fuer Neue Messung."""
+    datei = open(filename, 'r')
+
+    inhalt = datei.read()
+
+    t = inhalt.split('\n')
+
+    if status == 'neu':
+    # fuer tags
+
+        day = t[1][0:2]
+        month = t[1][3:5]
+        year = t[1][6:10]
+        uhrzeit = t[5][0:8].replace('.', ':')
+    else:
+        day = t[10][13:15]
+        month = t[10][10:12]
+        year = t[10][5:9]
+        uhrzeit = t[11][5:10]
+        
+
+    return year+'-'+month+'-'+day+' '+uhrzeit
 
 zaehler = 0
 list = []
 pfad = u'/home/ttaylan/Dokumente/Datenbank/NeueDaten/'
 dbname = 'test2'
 messung = 'neu1'
+client = DataFrameClient('localhost', 8086, 'root', 'root', dbname)
 os.chdir(pfad)
 
 # list aller vorhandener ordner
@@ -39,8 +72,9 @@ for user in ordner:
 
         filename = test.replace('.lvm', '.csv').replace('\xd6', 'oe')\
             .replace('\xfc', 'ue').replace('\xe4', 'ae')
-        client = DataFrameClient('localhost', 8086, 'root', 'root', dbname)
+        
         tag = os.path.basename(filename).replace('.csv', '')
+
         if os.path.isfile(filename):
             pass
         else:
@@ -48,18 +82,17 @@ for user in ordner:
 
         # Abfrage ob Datei shon in Datenbank vorhanden
 
-        query_string = "select COP from neu1 where Filename=\'%s\'" % tag
-        # zum debugggen
-        # print query_string
-        # fehler handling datenbank
-
         try:
-            check = client.query(query_string)
+            query_str = abfrage_datenbank(filename, messung, tag)
+            
+            check = client.query(query_str)
+
         except:
             check = True
         # wenn leer (tag nicht vorhanden)
         if bool(check) is False:
             # oeffnen csv und auslesen inhalt
+
             sr = open(dateiname, "rb")
             in_txt = csv.reader(sr, delimiter='\t')
             output = open(filename, 'wb')
@@ -75,14 +108,14 @@ for user in ordner:
             t2 = inhalt2.split('\n')
             # Bedingung wenn alte Messung
             if t2[0] == 'LabVIEW Measurement\t\r':
-                pass
-
-                for row in in_txt:    
-                    if index==24:
-                        laenge=len(row)
+                
+                for row in in_txt:
+                    if index == 24:
+                        laenge = len(row)
 
                         for spalte in row:
-                            conv = spalte.replace('\xd6','oe').replace('\xfc','ue').replace('\xb3','3')
+                            conv = spalte.replace('\xd6', 'oe')\
+                                .replace('\xfc', 'ue').replace('\xb3', '3')
 
                             a.append(conv)
 
@@ -93,77 +126,51 @@ for user in ordner:
                         del row[laenge-1]
                         writer.writerow(row)
                     else:
-                        writer.writerow(row)     
+                        writer.writerow(row)
 
                     index = index+1
 
-                datei = open(filename, 'r')
-
-                inhalt=datei.read()
-
-                t=inhalt.split('\n')
-
-
-
-                day=t[10][13:15]
-                month=t[10][10:12]
-                year=t[10][5:9]
-                uhrzeit=t[11][5:10]
-                datumzeit=year+'-'+month+'-'+day+' '+uhrzeit
-                datei2=pd.read_csv(filename, skiprows=24,index_col=False).fillna(0)
-                #datei2=pd.read_csv(filename, skiprows=3,index_col=False).fillna(0)
-                
                 try:
-                    datei2=pd.read_csv(filename, skiprows=24,index_col=False).fillna(0)
+                    datumzeit = find_datumzeit(filename, 'alt')
+                    datei2 = pd.read_csv(filename,
+                                         skiprows=24,
+                                         index_col=False).fillna(0)
                     try:
                         del datei2['X_Value']
                     except:
                         pass
-                    zeiti=pd.date_range(datumzeit, periods=len(datei2),freq='S')
-                    datei2.index=zeiti
+                    zeiti = pd.date_range(datumzeit,
+                                          periods=len(datei2),
+                                          freq='S')
+                    datei2.index = zeiti
                     data = pd.DataFrame(datei2, index=zeiti)
-                    
-                    
-                    
-                    client.write_points(data, messung,{'Filename': tag, 'User':user})
+
+                    client.write_points(data, messung, {'Filename': tag,
+                                                        'User': user})
                     print 'alt erfolgreich! %s' % (filename)
-                    zaehler=zaehler+1
-                except ValueError:
-                
-                    
-                    print 'DBError:     %s'%(filename)
+                    zaehler = zaehler+1
+                except:
+
+                    print 'DBError:     %s' % (filename)
 
             else:
-                # NEUE MESSUNG
+                # NEUE MESSUNG############################################
                 for row in in_txt:
                     if row:
                         writer.writerow(row)
                 sr.close()
                 output.close()
 
-                datei = open(filename, 'r')
-
-                inhalt = datei.read()
-
-                t = inhalt.split('\n')
-                # fuer tags
-                tagsi = t[0]
-                tags = tagsi.replace('\r', '').split(',')
-
-                day = t[1][0:2]
-                month = t[1][3:5]
-                year = t[1][6:10]
-                uhrzeit = t[5][0:8].replace('.', ':')
-                datumzeit = year+'-'+month+'-'+day+' '+uhrzeit
-
-                print datumzeit
                 try:
+                    datumzeit = find_datumzeit(filename, 'neu')
+                    print datumzeit
                     datei2 = pd.read_csv(filename, skiprows=3, index_col=False)
 
-                    datei2 = datei2.fillna(0)\
-                        .replace([np.inf, -np.inf], np.nan)
+                    datei2 = datei2.replace([np.inf, -np.inf], np.nan)
 
                     datei2 = datei2.fillna(0)
+
+                    datei2 = datei2.astype('float64')
                     try:
 
                         del datei2['Zeit']
